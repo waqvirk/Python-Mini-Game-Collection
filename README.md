f3c908019b07f52fc497df3d28c90f1a16644dab
waqvirk <virkwaqarahmad@gmail.com>
2025-08-25 17:05:10 +0200

    updated README and initialize script

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Python-Mini-Game-Collection
+🎯 Learning Objectives
+
+1. Gain experience in structuring small Python programs.
+
+2. Learn how to use randomness, loops, and user input.
+
+3. Practice writing functions to organize code.
+
+4. Improve debugging and testing skills.
+
+5. Build confidence by finishing playable games.
+
+6. Practice coding in teams.
+
+7. Improve git and github flow use.
---
+++ script.py
@@ -0,0 +1,32 @@
+# Work off the platform in groups using the following code as a base
+
+import random
+
+# NUMBER GUESSING GAME
+def number_guessing_game():
+
+
+
+# PAPER, ROCK, SCISSORS
+def rock_paper_scissors():
+
+
+# TIC-TAC-TOE
+# Function to print the board
+def print_board(board):
+
+
+# Function to check the winner
+def check_winner(board):
+
+
+# Function to run fo the actual tic-tac-toe game where your other functions will be used
+def tic_tac_toe():
+   
+
+# MENU SYSTEM
+def main_menu():
+    
+
+    
+# RUN THE MENU
